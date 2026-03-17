53d137101e3d10f54433ff920fa04498746b54b2
pico <monacogianluca110@gmail.com>
2026-03-17 16:23:34 +0100

    Update tickets.js

--- config/tickets.js
+++ config/tickets.js
@@ -1,1 +1,29 @@
+module.exports = {
+  PANEL_CHANNEL_ID: "1483483540153831519",
+  LOG_CHANNEL_ID: "1483483895956766914",
+  STAFF_ROLE_ID: "1483484839867977868",
 
+  TYPES: {
+    supporto: {
+      label: "Supporto",
+      emoji: "🔵",
+      name: "supporto",
+      buttonStyle: 1, // Primary (blu)
+      description: "Hai bisogno di aiuto o assistenza generale."
+    },
+    partnership: {
+      label: "Partnership",
+      emoji: "🟡",
+      name: "partnership",
+      buttonStyle: 2, // Secondary (usiamo emoji gialla)
+      description: "Richieste partnership, collaborazioni o proposte."
+    },
+    segnalazione: {
+      label: "Segnalazione",
+      emoji: "🔴",
+      name: "segnalazione",
+      buttonStyle: 4, // Danger (rosso)
+      description: "Segnala problemi, utenti o situazioni da controllare."
+    }
+  }
+};
