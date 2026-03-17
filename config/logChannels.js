eb007accf16d33f5296c24e1ba72a83a9acee38d
pico <monacogianluca110@gmail.com>
2026-03-17 14:15:46 +0100

    Update logChannels.js

--- config/logChannels.js
+++ config/logChannels.js
@@ -1,8 +1,14 @@
 module.exports = {
-  memberLog: "1455212222732046391",
+  memberLog: "14552122227232046391",
   messageLog: "1455213585176199262",
   vcLog: "1455212297378201773",
   modLog: "1455212328336228393",
   inviteLog: "1455212706977157203",
   serverLog: "1455214028170334278",
+
+  // opzionali extra
+  roleLog: "",
+  channelLog: "",
+  emojiLog: "",
+  stickerLog: ""
 };
